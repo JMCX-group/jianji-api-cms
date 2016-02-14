bf4b8332aebe0608fe3282c26e799c3b4f59c6b3
lyx <44600937@qq.com>
2016-02-14 11:39:00 +0800

    修改数据库配置

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'host'      => 'lifejianji.com',
+            'database'  => 'jianji',
+            'username'  => 'root',
+            'password'  => 'JMCX-mysql2016',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
