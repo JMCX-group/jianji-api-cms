53a366fd125dd51d4fa566e905df04ab030748df
lyx <44600937@qq.com>
2016-02-17 14:57:48 +0800

    修改数据库结构

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,23 +15,26 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
 
-            $table->string('nickname');
-            $table->string('real_name');
-            $table->string('phone')->nullable();
-            $table->string('email')->unique();
+            $table->string('nickname')->nullable();
+            $table->string('real_name')->nullable();
+            $table->string('phone');
+            $table->string('email')->nullable();
             $table->string('password', 60);
-            $table->string('head_img_url');
-            $table->string('signature');
-            $table->string('sex')->default('1');
-            $table->integer('age');
-            $table->string('address');
-            $table->string('tag');
-            $table->string('psychological_test');
+            $table->string('head_img_url')->nullable();
+            $table->string('signature')->nullable();
+            $table->string('sex')->nullable();
+            $table->integer('age')->nullable();
+            $table->string('address')->nullable();
+            $table->string('tag')->nullable();
+            $table->string('psychological_test')->nullable();
             $table->dateTime('time');
-            $table->dateTime('last_login_time');
+            $table->dateTime('last_login_time')->nullable();
 
             $table->rememberToken();
             $table->timestamps();
+
+            $table->unique('phone');
+            $table->index('email');
         });
     }
 
